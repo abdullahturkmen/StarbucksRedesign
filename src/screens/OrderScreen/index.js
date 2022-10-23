3a0af18f5580dcc99ae385391bba292fbd2dd56e
Abdullah Turkmen <abdullahtrkmn13@gmail.com>
2022-10-23 22:12:16 +0300

    coffee menu json added and listed in order screen

--- src/screens/OrderScreen/index.js
+++ src/screens/OrderScreen/index.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
     SafeAreaView,
     StatusBar,
@@ -14,45 +14,39 @@ import {
     useColorScheme,
     View,
     Button,
-    Header
+    Image,
+    ScrollView
 } from 'react-native';
+
+import { getMenuData } from './menuData';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import OrderScreenStyles from './style';
 
-const Section = ({children, title}) => {
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-        <View style={
-            OrderScreenStyles.sectionContainer
-        }>
-            <Text style={
-                [
-                    OrderScreenStyles.sectionTitle, {
-                        color: isDarkMode ? Colors.white : Colors.black
-                    },
-                ]
-            }>
-                {title} </Text>
-            <Text style={
-                [
-                    OrderScreenStyles.sectionDescription, {
-                        color: isDarkMode ? Colors.light : Colors.dark
-                    },
-                ]
-            }>
-                {children} </Text>
-        </View>
-    );
-};
+const menuDataItem = (details) => {
+    return (<View style={{display: 'flex',flexDirection: 'row',marginBottom: 10}}>
+    <Image style={{width: 80, height: 80, borderRadius: 100, marginRight: 20}} source={{uri: details['image']}} />
+    <View style={{flex: 1}}>
+    <Text style={{color: '#2E2D38', fontSize: 20, fontWeight: 'bold'}}>{details['title']}</Text>
+    <Text style={{color: '#2E2D38'}}>20 TL</Text>
+    <View style={{display: 'flex', justifyContent: 'flex-end'}}><Button style={{width: 'max-content'}} title='Ekle'></Button></View>
+    </View>
+</View>)
+}
 
 const OrderScreen = ({navigation}) => {
+    const [scrollMenu, setScrollMenu] = useState('all');
+    const [dataState, setDataState] = useState([]);
     const isDarkMode = useColorScheme() === 'dark';
 
     const backgroundStyle = {
         backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
     };
+
+    useEffect(() => {
+        getMenuData().then(setDataState);
+      }, []);
 
     return (
         <SafeAreaView style={backgroundStyle}>
@@ -63,25 +57,56 @@ const OrderScreen = ({navigation}) => {
 
 
             <View style={
-                {
-                    backgroundColor: "#F7F8FA",
-                }
+                {backgroundColor: "#F7F8FA"}
             }>
-                <Text>This is 's profile</Text>
-                <Section title="OrderScreen">
-                    Edit
-                    <Text style={
-                        OrderScreenStyles.highlight
-                    }>Order Screen</Text>
-                    to change this
-                                screen and then come back to see your edits.
-                </Section>
+                
 
-                <Button title="Go to LoginScreen"
-                    onPress={
-                        () => navigation.navigate('Login')
-                    }/>
-                    
+                <View style={
+                    {
+                        paddingVertical: 20,
+                        overflow: 'hidden'
+                    }
+                }>
+                    <Text>{scrollMenu}</Text>
+                    <ScrollView horizontal={true}>
+
+                        <Button title="all"
+                            onPress={
+                                () => setScrollMenu('all')
+                        }></Button>
+                        {dataState.map(e => (
+                           
+                            <Button title={e.categoryName}
+                            onPress={
+                                () => setScrollMenu(e.categoryName)
+                        }></Button>
+                        ))}
+                        
+                        
+
+                    </ScrollView>
+
+
+                </View>
+
+                <View>
+                    <ScrollView style={{padding: 10}}>
+                        { scrollMenu === "all" ?
+                             dataState.map(e => (
+                                e.items.map(x => (         
+                                    menuDataItem(x)
+                                ))
+                            ))
+                        : 
+                             dataState.filter(e => e.categoryName == scrollMenu).map(e => (
+                                e.items.map(x => (
+                                    menuDataItem(x)
+                                ))
+                            ))
+                        }
+                    </ScrollView>
+                </View>
+
 
             </View>
 
